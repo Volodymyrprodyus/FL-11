17de158b3e8c6dc4c47c1f50772b42a1c3b1bdfb
Volodymyr Prodyus <volpro86@gmail.com>
2019-09-03 23:39:39 +0300

    Add files via upload

---
+++ FL_11_20_REACT_JSX/emoji_react/src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Pack from './pages/pack/pack.component';
+
+function App() {
+    return (
+      <div className="Container">
+          < Pack />
+      </div>
+    );
+  }
+  
+export default App;
